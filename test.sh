d047566f6f94320dbbe8af7ce65bd9783e2ec7aa
Mitar <mitar.git@tnode.com>
2023-07-12 00:18:15 +0200

    Add CI test.

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+cleanup() {
+  set +e
+
+  echo "Logs"
+  docker logs test
+
+  echo "Stopping Docker image"
+  docker stop test
+  docker rm -f test
+}
+
+echo "Running Docker image"
+docker run -d --name test -e LOG_TO_STDOUT=1 -p 53:53 "${CI_REGISTRY_IMAGE}:${TAG}"
+trap cleanup EXIT
+
+echo "Sleeping"
+sleep 10
+
+echo "Testing"
+nc -z docker 53
+echo "Success"
